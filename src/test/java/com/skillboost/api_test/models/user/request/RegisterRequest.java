d36a7cedefeb3a372228a01067a87dc465cefb5f
Kwame Oduro <odurokwameee@gmail.com>
2025-11-20 15:32:25 +0000

    feat: add register request for user registration api requests

---
+++ src/test/java/com/skillboost/api_test/models/user/request/RegisterRequest.java
@@ -0,0 +1,13 @@
+package com.skillboost.api_test.models.user.request;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+    private String email;
+    private String password;
+}
+
